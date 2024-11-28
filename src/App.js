5ef8f5e7767673f39ecfa172c9e9b9525abc565b
Akash Kumar <akash.singh1200@gmail.com>
2024-11-28 14:27:17 +0530

    intiial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './navbar/navbar';
+import Home from './pages/home';
+import SymptomChecker from './pages/symptom/symptom';
+// import SymptomChecker from './pages/symptom';
+// import SymptomChecker from './pages/symptomChecker';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-container'>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/symptom-checker" element={<SymptomChecker />} />
+      </Routes>
+    </Router>
     </div>
   );
 }
